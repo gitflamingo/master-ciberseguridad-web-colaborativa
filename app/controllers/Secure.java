6a38cd2dd9cd5cef0afba6e09bdecd3ff8c60c39
gitflamingo <92251234+gitflamingo@users.noreply.github.com>
2021-12-21 13:08:35 -0600

    Encriptación de contraseña al autenticar
    Encriptación de contraseña md5 al autenticar

--- app/controllers/Secure.java
+++ app/controllers/Secure.java
@@ -17,6 +17,22 @@ public class Secure extends Controller {
         login();
     }
 
+            public string encryption(String password)  
+        {  
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();  
+            byte[] encrypt;  
+            UTF8Encoding encode = new UTF8Encoding();  
+    //encrypt the given password string into Encrypted data  
+            encrypt = md5.ComputeHash(encode.GetBytes(password));  
+            StringBuilder encryptdata = new StringBuilder();  
+    //Create a new string by using the encrypted data  
+            for (int i = 0; i < encrypt.Length; i++)  
+            {  
+                encryptdata.Append(encrypt[i].ToString());  
+            }  
+            return encryptdata.ToString();  
+        }  
+
     public static void authenticate(String username, String password){
         User u = User.loadUser(username);
         if (u != null && u.getPassword().equals(HashUtils.getMd5(password))){
